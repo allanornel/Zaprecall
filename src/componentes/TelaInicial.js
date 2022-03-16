c7013c8934f86eb5011bd3f348ff4398593ea609
allanornel <allanornel@gmail.com>
2022-03-16 15:57:58 -0300

    Feat: layout tela1 done

---
+++ src/componentes/TelaInicial.js
@@ -0,0 +1,16 @@
+export default function TelaInicial() {
+  return (
+    <>
+      <div className="tela1">
+        <div className="titulo-inicio">
+          <img src="./assets/img/logo.png" alt="logo"></img>
+          <h1>ZapRecall</h1>
+        </div>
+
+        <div className="inicio-recall">
+          <p>Iniciar Recall!</p>
+        </div>
+      </div>
+    </>
+  );
+}
